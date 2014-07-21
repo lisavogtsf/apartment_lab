a3b0383af268a4f4ae5e315418578abb8e1bb788
Lisa Vogt <lisavogt.sf@gmail.com>
2014-07-20 17:58:22 -0700

    passing, still no property methods

--- src/property_types/property.js
+++ src/property_types/property.js
@@ -1,4 +1,5 @@
-var app = require('../main.js')
+var app = require('../main.js');
+
 function Property(address) {
   this.address = address,
   this.manager = [],
@@ -35,6 +36,27 @@ Property.prototype.addTenant = function(unit, tenant) {
   // }
 
 };
+
+// Property.prototype.setManager = function(person) {
+//   // only add if there is no manager, and 
+//   // person is a manager
+
+//   // code below breaking tests?
+//   if (this.manager.length < 1 && person instanceof app.Manager){
+//     this.manager = person;    
+//   } else {
+//     console.log("Either this person is not a Manager or this building already has a Manager.");
+//     return -1;
+//   }
+
+// };
+
+// Property.prototype.getManager = function(){
+//   // return this.manager 
+//   // ** error if the property has no manager
+//   return this.manager.name;
+// };
+
 
 Property.prototype.removeTenant = function(unit, tenant) {
   // remove tenant
--- test/paste_test.js
+++ test/paste_test.js
@@ -8,36 +8,12 @@
 // var TownHouse = app.TownHouse;
 // var Duplex = app.Duplex;
 
-// var testProperty = new Property("testAddress");
 // var roper = new Manager("roper", "333-3333");
 // var jenny = new Tenant("jenny", "867-5309");
+// var paramount = new Property("paramount address");
+// var brownstone = new TownHouse("brownstone address");
+// var testProperty = new Property("testAddress");
 
-// // Property
+// roper.addProperty(paramount);
+// roper.addProperty(brownstone);
 
-// function Property(address) {
-//   this.address = address;
-//   this.manager = [];
-//   this.tenants = [];
-//   this.units = [];
-//   this.numUnits = 1;
-// }
-
-// Property.prototype.setManager = function(person) {
-//   // only add if there is no manager, and 
-//   // person is a manager
-
-//   // code below breaking tests?
-//   if (this.manager.length < 1 && person instanceof app.Manager){
-//     this.manager = person;    
-//   } else {
-//     console.log("Either this person is not a Manager or this building already has a Manager.");
-//     return -1;
-//   }
-
-// };
-
-// Property.prototype.getManager = function(){
-//   // return this.manager 
-//   // ** error if the property has no manager
-//   return this.manager.name;
-// };
