4446b950b160fe2cb25f226377b2d8e13bd3e973
Lisa Vogt <lisavogt.sf@gmail.com>
2014-07-20 21:59:47 -0700

    text for pasting into texts

--- test/paste_test.js
+++ test/paste_test.js
@@ -1,4 +1,6 @@
-// big file to paste into node for testing
+// big file to paste snippets into node for testing
+// since I can't get my files to work together
+// or I don't understand inheritance
 
 // var app = require('./src/main.js');
 // var Person = app.Person;
@@ -11,64 +13,71 @@
 // var roper = new Manager("roper", "333-3333");
 // var jenny = new Tenant("jenny", "867-5309");
 
-// var paramount = new Property("paramount address");
+// var joe = new Person("joe");
+// var bob = new Person("bob");
+// jenny.addReference(joe);
+// jenny.addReference(bob);
+
+
 // var brownstone = new TownHouse("brownstone address");
+
+// var paramount = new Property("paramount address");
 // var testProperty = new Property("testAddress");
 // var home = new Property("777 4th Ave");
-
+// var place = new Property("here");
 
 // roper.addProperty(paramount);
 // roper.addProperty(brownstone);
 
-//***************************
+// //***************************
 
-function Person(name, contact){
-  this.name = name,
-  this.contact = contact
-};
+// function Person(name, contact){
+//   this.name = name,
+//   this.contact = contact
+// };
 
-var inherits = function(Child, Parent){
-  Child.prototype = new Parent(),
-  Child.prototype.constructor = Child
-}
+// var inherits = function(Child, Parent){
+//   Child.prototype = new Parent(),
+//   Child.prototype.constructor = Child
+// }
 
-function Property(address) {
-  this.address = address,
-  this.manager = [],
-  this.tenants = [],
-  this.units = [],
-  this.maxUnits = 1
-};
+// function Property(address) {
+//   this.address = address,
+//   this.manager = [],
+//   this.tenants = [],
+//   this.units = [],
+//   this.maxUnits = 1
+// };
 
-Property.prototype.setManager = function(person){
-  this.manager = person;
-};
+// Property.prototype.setManager = function(person){
+//   this.manager = person;
+// };
 
-function Manager(name, contact) {
-  this.name = name,
-  this.contact = contact,
+// function Manager(name, contact) {
+//   this.name = name,
+//   this.contact = contact,
   
-  this.properties = []
-}
-// 
-inherits(Person, Manager);
+//   this.properties = []
+// }
+// // 
+// inherits(Person, Manager);
 
-Manager.prototype.addProperty = function(property) {
-  // ** may need to check that there is not already
-  // a manager of the property
-  this.properties.push(property);
-};
+// Manager.prototype.addProperty = function(property) {
+//   // ** may need to check that there is not already
+//   // a manager of the property
+//   this.properties.push(property);
+// };
 
-Manager.prototype.removeProperty = function(property) {
-  // ** need to check whether the manager has this 
-  // property in the first place
+// Manager.prototype.removeProperty = function(property) {
+//   // ** need to check whether the manager has this 
+//   // property in the first place
 
-  var oldProperty = this.properties.indexOf(property);
-  // return value is for checking
-  var removed = this.properties.splice(oldProperty, 1);
-  return removed;
+//   var oldProperty = this.properties.indexOf(property);
+//   // return value is for checking
+//   var removed = this.properties.splice(oldProperty, 1);
+//   return removed;
 
-};
+// };
 
 
 
